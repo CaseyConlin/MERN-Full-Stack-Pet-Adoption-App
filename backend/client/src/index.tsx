7356c81f517f9c8eb8539b9d7e3643fd4823f786
Casey Conlin <101153261+CaseyConlin@users.noreply.github.com>
2023-02-11 11:26:12 -0500

    Configure tailwind for production.

--- backend/client/src/index.tsx
+++ backend/client/src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { CartState } from "./context/cartState";
+import "./index.css";
 import App from "./App";
-
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
--- backend/client/tailwind.config.js
+++ backend/client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".src/components/**/*.{html,js}"],
+  content: ["src/components/*.{html,tsx,jsx}", "src/*.{html,tsx,jsx}"],
+
   theme: {
     extend: {},
   },
